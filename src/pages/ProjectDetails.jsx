2b8922d26f52f22917d85e7cb193e59ba916f8f1
adeeltahirdev <adeeltahir0301@gmail.com>
2026-09-24 21:48:15 +0500

    Created project detail page and attached it to the view btn

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,8 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useProjects } from "../context/ProjectContext";
 
 function ProjectDetails() {
-    return(
-        <h1>Project Details</h1>
+  const { projectId } = useParams();
+  const { projects } = useProjects();
+
+  const idInt = parseInt(projectId, 10);
+
+  const project = projects.find((p) => p.id === idInt);
+
+    if (!project) {
+    return (
+      <>
+        <div className="header">
+          <h1>Project Details</h1>
+        </div>
+
+        <div className="project-details">
+          <h2 className="name">Project not found</h2>
+          <p className="description">
+            This project doesn't exist or may have been deleted.
+          </p>
+        </div>
+      </>
     );
+  }
+
+  return (
+    <>
+      <div className="header">
+        <h1>Project Details</h1>
+      </div>
+
+      <div className="project-details">
+        <h2 className="name">{project.name}</h2>
+        <p className="description">{project.description}</p>
+      </div>
+    </>
+  );
 }
 
-export default ProjectDetails
+export default ProjectDetails;
