708af876aa2ee7ddd681a375b97ae1cab8dd9da4
adeeltahirdev <adeeltahir0301@gmail.com>
2026-09-24 12:00:38 +0500

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import ForgotPassword from './pages/ForgotPassword.jsx'
+import ProjectDetails from './pages/ProjectDetails.jsx'
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/login', element: <Login />},
+  {path: '/register', element: <Register />},
+  {path: '/forgot-password', element: <ForgotPassword />},
+  {path: '/dashboard', element: <Dashboard />},
+  {path: '/projects/:projectId', element: <ProjectDetails />}
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
---
+++ src/pages/ProjectDetails.jsx
@@ -0,0 +1,8 @@
+
+function ProjectDetails() {
+    return(
+        <h1>Project Details</h1>
+    );
+}
+
+export default ProjectDetails
